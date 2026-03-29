272b0368afec1311b1c24d520905d1c3cbb47910
Jairo Cabrera <jairocabrera87@hotmail.com>
2026-03-28 20:27:30 -0500

    Adding logs and other files to the pipeline

---
+++ data-orquestador/orquestador/data_exporters/load_data.py
@@ -0,0 +1,61 @@
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+from mage_ai.settings.repo import get_repo_path
+from mage_ai.io.config import ConfigFileLoader
+from mage_ai.io.postgres import Postgres
+
+from pandas import DataFrame
+from os import path
+import math
+from tqdm.auto import tqdm
+
+@data_exporter
+def export_data(data, *args, **kwargs):
+    """
+    Exports data to some source.
+
+    Args:
+        data: The output from the upstream parent block
+        args: The output from any additional upstream blocks (if applicable)
+
+    Output (optional):
+        Optionally return any object and it'll be logged and
+        displayed when inspecting the block run.
+    """
+    if not data
+        return False
+    year = kwargs.get('year')
+    month = kwargs.get('month')
+    table_name = f'ny_taxi_trips_{year}_{month}'
+
+    schema_name = 'raw'
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+    config_profile = 'default'
+
+    size = 100000
+
+    start = 0
+    end = size
+    num_chunks = math.ceil(len(data)/size)
+    
+    with Postgres.with_config(ConfigFileLoader(config_path, config_profile)) as loader:
+        loader.export(
+            data.iloc[start:end],
+            schema_name,
+            table_name,
+            index=False,
+            if_exists='replace'
+        )
+        start = end
+        end = min(end + size, len(data))
+        for i in tqdm(range(1, num_chunks), desc="Cargando datos", unit="chunk"):
+            loader.export(
+                data.iloc[start:end],
+                schema_name,
+                table_name,
+                index=False,
+                if_exists='append'
+            )
+            start = end
+            end = min(end + size, len(data))
---
+++ data-orquestador/orquestador/data_loaders/extract_data.py
@@ -0,0 +1,59 @@
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+from mage_ai.io.config import ConfigFileLoader
+from mage_ai.io.postgres import Postgres
+import pandas as pd
+
+@data_loader
+def load_data(*args, **kwargs):
+    """
+    Template code for loading data from any source.
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    year = kwargs.get('year')
+    month = kwargs.get('month')
+    schema = kwargs.get('schema')
+    config_path = kwargs.get('config_path')
+    config_profile = kwargs.get('config_profile')
+    if not year or not month or not schema or not config_path or not config_profile:
+        raise ValueError("Missing year or month or schema")
+        
+    table_name = f'ny_taxi_trips_{year}_{month}'
+    exists = False
+    with Postgres.with_config(ConfigFileLoader(config_path, config_profile)) as loader:
+        loader.open()
+        conn = loader.conn
+        cur = conn.cursor()
+
+        cur.execute(f"""
+                    SELECT EXISTS (
+                        SELECT 1
+                        FROM information_schema.tables 
+                        WHERE table_schema = '{schema}'
+                        AND table_name = '{table_name}'
+                    );
+                """)
+
+        exists = cur.fetchone()[0]
+        cur.close()
+
+    if exists:
+        print("Table already populated, skipping")
+        return False
+    
+    base_url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_year-month.parquet"  
+    url = base_url.replace('year', year).replace('month', month)
+    return pd.read_parquet(url)
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
